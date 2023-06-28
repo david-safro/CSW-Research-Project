259ca49164d83847f4acc2db0eb653acb92e2e37
Glitchez <87347098+Glitchez-1984@users.noreply.github.com>
2023-06-27 23:31:54 -0400

    Update main.py

--- neural network/src/main.py
+++ neural network/src/main.py
@@ -1,10 +1,15 @@
 import sys
-sys.path.append('/neural network/data/')
-from data import HeartDiseasePrediction
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from constants import *
+# Add the parent directory of the neural network folder to the module search path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Usage example:
+from data.data import HeartDiseasePrediction
 csv_file = 'Heart_Disease_Prediction.csv'
-prediction = HeartDiseasePrediction(csv_file)
+prediction = HeartDiseasePrediction()
 prediction.read_data()
 prediction.preprocess_data()
 prediction.split_data()
@@ -16,5 +21,41 @@ print(prediction.data.head())
 print("Scaled features:")
 print(prediction.X_scaled[:5])
 
-print("Training set shape:", prediction.X_train.shape, prediction.y_train.shape)
+print("Training set shape"":", prediction.X_train.shape, prediction.y_train.shape)
 print("Testing set shape:", prediction.X_test.shape, prediction.y_test.shape)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+
+
+
+X_train = X_train.to(device)
+
+def main():
+    model = NeuralNetwork.to(device)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.SGD(params = NeuralNetwork.parameters(), lr=LR)
+    torch.manual_seed(42)
+    for epoch in range(EPOCH_COUNT):
+        model.train()
+        optimizer.zero_grad()
+        optimizer.step()
+        #train loop
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch [{epoch + 1}/{EPOCH_COUNT}], Loss: {loss_fn.item():.4f}')
+
+
+if __name__ == '__main':
+    main()
